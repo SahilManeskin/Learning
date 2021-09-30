2fe59328d4a13f74f426a749537a57d7e0156deb
Sahil <sahilsoni1516@gmail.com>
2021-09-30 17:39:11 +0530

    Test

--- learning/java/DisplayCharacter.java
+++ learning/java/DisplayCharacter.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class DisplayCharacter {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        
+
         sc.close();
+        //kndkn"
     }
 }
